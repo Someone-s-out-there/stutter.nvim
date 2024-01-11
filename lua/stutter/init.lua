8ef53660d6b42d799b916accbab4ba933242ec63
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-01-11 15:57:08 +0100

    added 2nd version of the parser which can parse multiple types of mappings also added calback funtion which gets passed into vim.on_key

--- lua/stutter/init.lua
+++ lua/stutter/init.lua
@@ -9,6 +9,27 @@ local function parser(char)
     end
 end
 
+local function parser2(char)
+    for _, pattern in pairs(options.patterns) do
+        if prevchars:endswith(pattern[1]) then
+            vim.api.nvim_input("<BS><BS>")
+            vim.api.nvim_input(pattern[2])
+        end
+    end
+end
+
+
+local function onkeycallback(char)
+    if not stutterState and not vim.api.nvim_get_mode().mode == "i" then
+        return
+    end
+    prevchars = prevchars .. char
+    parser2(char)
+    if string.len(prevchars) >= prevchars_count then
+        prevchars = string.sub(prevchars, -5)
+    end
+end
+
 function M.setup(opts)
     commands.command_creation()
 
