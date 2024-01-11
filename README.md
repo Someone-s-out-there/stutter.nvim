4356ba862cbe5b696102dc4fd440eddb158f725f
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-01-11 16:53:17 +0100

    updated README

--- README.md
+++ README.md
@@ -4,13 +4,26 @@ This plugin for neovim allows the mapping of key combinations in insert mode to 
 
 ## Installation
 
-```lua 
+``` lua
 use {'Someone-s-out-there/stutter.nvim'}
 
-require('stutter').setup({})
+require('stutter').setup({
+    {"{pattern}","{replacement}"},
+    })
 
-``` 
+```
 
 ## Usage
 
+if you want a mapping like `;;` => `:` as a sub map of `;;;`=> `:=` 
+you should pass it into the setup like the following
+
+``` lua
+
+require('stutter').setup({
+    {";;",":"},
+    {":;",":="},
+    })
+
+```
 
--- lua/stutter/init.lua
+++ lua/stutter/init.lua
@@ -4,34 +4,37 @@ local commands = require("stutter.commands")
 local M = {}
 local options = {}
 local prevchars = ""
-local prevchars_count = 5
+local prevchars_count = 10
 local stutterState = true
-
-local function parser(char)
-    if prevchars:endswith(options.patterns[1][1]) then
-        vim.api.nvim_input("<BS><BS>test")
-    end
-end
+local uptodate= false
 
 local function parser2(char)
     for _, pattern in pairs(options.patterns) do
         if prevchars:endswith(pattern[1]) then
             vim.api.nvim_input("<BS><BS>")
             vim.api.nvim_input(pattern[2])
+            uptodate = true
         end
     end
 end
 
-
 local function onkeycallback(char)
-    if not stutterState and not vim.api.nvim_get_mode().mode == "i" then
+    if not stutterState and not vim.api.nvim_get_iode().mode == "i" then
         return
     end
+
     prevchars = prevchars .. char
-    parser2(char)
-    if string.len(prevchars) >= prevchars_count then
-        prevchars = string.sub(prevchars, -5)
+
+    if prevchars:endswith("kb") then
+        prevchars = string.sub(prevchars, 1,-5)
     end
+
+    parser2(char)
+
+    if string.len(prevchars) >= prevchars_count then
+        prevchars = string.sub(prevchars, -10)
+    end
+
 end
 
 function M.setup(opts)
