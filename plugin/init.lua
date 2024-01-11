9182ae361d572acb68d40708c31cd02d22572bc4
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-01-11 13:35:01 +0100

    added framework for commands to start and stop stutter mode

--- plugin/init.lua
+++ plugin/init.lua
@@ -4,5 +4,10 @@ vim.on_key(function(char)
     print(char)
 end)
 
+vim.api.nvim_create_user_command("Stutter-on", function (opts)
+    print("stutter on")
+end, {nargs = 0})
 
- 
+vim.api.nvim_create_user_command("Stutter-off", function (opts)
+    print("stutter off")
+end, {nargs = 0})
