03ea736db6110e06f3f57c7f7bebdab02d8b4f3f
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-01-11 15:58:13 +0100

    added stutterstate var to table

--- lua/stutter/commands.lua
+++ lua/stutter/commands.lua
@@ -1,12 +1,14 @@
-local M = {}
+local M = { stutterstate = false }
 
 function M.command_creation()
     vim.api.nvim_create_user_command("StutterOn", function(opts)
         print("stutter on")
+        M.stutterstate = true
     end, { nargs = 0 })
 
     vim.api.nvim_create_user_command("StutterOff", function(opts)
         print("stutter off")
+        M.stutterstate = false
     end, { nargs = 0 })
 end
 
