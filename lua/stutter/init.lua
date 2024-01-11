717c665d7e36fa1dcbf50d5250122162ff90e7fb
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-01-11 21:27:28 +0100

    move parser to parser.lua and imported it into init.lua

--- lua/stutter/init.lua
+++ lua/stutter/init.lua
@@ -1,20 +1,12 @@
 require("stutter.string_util")
 local command = require("stutter.commands")
+local parser = require("stutter.parser")
 
 local M = {}
 
 local options = {}
 local prevchars = ""
 local prevchars_count = 10
-
-local function parser(char)
-    for _, pattern in pairs(options.patterns) do
-        if prevchars:endswith(pattern[1]) then
-            vim.api.nvim_input("<BS><BS>")
-            vim.api.nvim_input(pattern[2])
-        end
-    end
-end
 
 local function onkeycallback(char)
     if (not command.stutterstate) or (not vim.api.nvim_get_mode().mode == "i") then
@@ -27,7 +19,7 @@ local function onkeycallback(char)
         prevchars = string.sub(prevchars, 1, -5)
     end
 
-    parser(char)
+    parser.parser(prevchars, options)
 
     if string.len(prevchars) >= prevchars_count then
         prevchars = string.sub(prevchars, -10)
--- lua/stutter/parser.lua
+++ lua/stutter/parser.lua
@@ -1,2 +1,14 @@
 local M ={}
+
+---@param prevchars string
+---@param options table
+---@return nil
+function M.parser(prevchars,options)
+    for _, pattern in pairs(options.patterns) do
+        if prevchars:endswith(pattern[1]) then
+            vim.api.nvim_input("<BS><BS>")
+            vim.api.nvim_input(pattern[2])
+        end
+    end
+end
 return M
