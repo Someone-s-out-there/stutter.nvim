b2dfa2e3ebbc5dd2e6263c7676f6b1f4c0ad65b4
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-01-11 14:32:42 +0100

    vim on key function now passes all keys pressed to pareser

--- lua/stutter/init.lua
+++ lua/stutter/init.lua
@@ -11,5 +11,11 @@ end
 
 function M.setup(opts)
     commands.command_creation()
+
+    local stutterState = true
+    vim.on_key(function(char)
+        parser(char)
+    end)
+
     print("plugin/example.lua is executed!")
 end
