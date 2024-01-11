231a9344592a2b47677d4111d30e7cea4871a7f4
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-01-11 20:38:21 +0100

    minor refactoring

--- lua/stutter/init.lua
+++ lua/stutter/init.lua
@@ -1,45 +1,44 @@
 require("stutter.string_util")
-local commands = require("stutter.commands")
+local command = require("stutter.commands")
 
 local M = {}
+
 local options = {}
 local prevchars = ""
 local prevchars_count = 10
 local stutterState = true
-local uptodate= false
 
-local function parser2(char)
+local function parser(char)
     for _, pattern in pairs(options.patterns) do
         if prevchars:endswith(pattern[1]) then
             vim.api.nvim_input("<BS><BS>")
             vim.api.nvim_input(pattern[2])
-            uptodate = true
         end
     end
 end
 
 local function onkeycallback(char)
-    if not stutterState and not vim.api.nvim_get_iode().mode == "i" then
+    if not stutterState and not vim.api.nvim_get_mode().mode == "i" then
         return
     end
 
     prevchars = prevchars .. char
 
     if prevchars:endswith("kb") then
-        prevchars = string.sub(prevchars, 1,-5)
+        prevchars = string.sub(prevchars, 1, -5)
     end
 
-    parser2(char)
+    parser(char)
 
     if string.len(prevchars) >= prevchars_count then
         prevchars = string.sub(prevchars, -10)
     end
-
 end
 
 function M.setup(opts)
     options = opts
-    commands.command_creation()
+
+    command.command_creation()
 
     vim.on_key(onkeycallback)
 end
