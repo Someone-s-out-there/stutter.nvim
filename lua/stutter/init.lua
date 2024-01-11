e906df2f97efdbe65b9e6b4a67f6b1759bc7a9c5
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-01-11 14:32:12 +0100

    added parser function to parse the chars pressed

--- lua/stutter/init.lua
+++ lua/stutter/init.lua
@@ -1,6 +1,13 @@
 local commands = require("lua.stutter.commands")
 
 local M = {}
+local prevchar = nil
+
+local function parser(char)
+    if char == "#" and prevchar == "#" then
+        vim.api.nvim_input("<BS><BS>test")
+    end
+end
 
 function M.setup(opts)
     commands.command_creation()
