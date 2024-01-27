7c1457408e37922c9928e413b872764fb3d43bda
Sjoerd van de Wege <sjoerd.vandewege@gmail.com>
2024-01-27 21:12:23 +0100

    added global language support

--- lua/stutter/parser.lua
+++ lua/stutter/parser.lua
@@ -6,7 +6,7 @@ local M = {}
 ---@param buffer_lang string
 function M.parser(prevchars, options, buffer_lang)
     for _, pattern in pairs(options.patterns) do
-        if prevchars:endswith(pattern[1]) and buffer_lang == pattern[3] then
+        if prevchars:endswith(pattern[1]) and (buffer_lang == pattern[3] or pattern[3] == nil) then
             vim.api.nvim_input("<BS><BS>")
             vim.api.nvim_input(pattern[2])
         end
